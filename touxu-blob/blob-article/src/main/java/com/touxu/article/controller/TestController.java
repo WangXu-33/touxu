0486218b79de3cc5c78ba48dd40ffa68e6e0c0e5
王旭 <67303972+WangXu-33@users.noreply.github.com>
2022-05-04 17:17:38 +0800

    Test

--- touxu-blob/blob-article/src/main/java/com/touxu/article/controller/TestController.java
+++ touxu-blob/blob-article/src/main/java/com/touxu/article/controller/TestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String test(){
+        System.out.println("郭冰洁是个大蠢猪");
         return "hello world";
     }
 }
